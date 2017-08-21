6ac05f4d53556000cc814f898d42cc610addfbc2
Philippe PERON <peronzone@yahoo.fr>
2017-08-21 11:42:07 +0200

    init projet philippe

---
+++ inc/init.inc.php
@@ -0,0 +1,20 @@
+<?php
+// connexion � la base de donn�es
+$pdo = new PDO('mysql:host=localhost;dbname=timemachine','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,  PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
+require_once("function.inc.php");
+
+//cr�ation de variables pouvant nous servir dans le cadre du projet:
+//variable pour afficher des messages � l'utilisateur.
+$message="";
+
+//ouverture de la session
+session_start();
+
+
+// d�finition de la constante pour le chemin absolu ainsi que pour la racine serveur
+// racine site
+define("URL","/projetfinalsitephoto/");
+
+//racine serveur
+define("RACINE_SERVEUR", $_SERVER['DOCUMENT_ROOT'] . URL);
